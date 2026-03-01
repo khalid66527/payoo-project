f6f7527d36cbca67d8cf8a0e5853eba4cc86090a
khalid hasan <khalidhasan678954321@gmail.com>
2026-03-01 15:33:57 +0600

    login and cashout maachine diye

---
+++ script/login.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-login')
+    .addEventListener('click', function () {
+        //   step-1 get mbile number 
+        const numberInput = document.getElementById('input-number');
+        const contactNumber = numberInput.value;
+        console.log(contactNumber);
+        // step -2 get the pin input
+        const pinInput = document.getElementById('input-pin');
+        const Pin = pinInput.value;
+        console.log(Pin);
+        // step-3 match pic &number number
+        if(contactNumber=='01234567890'&&'1234'){
+            // step-3-1 true::>>  alert homepage
+            alert('login Success')
+            // window.location.replace('/homepage.html')
+            window.location.assign('/homepage.html')
+        }
+        else{
+            alert('login Failled');
+            return;
+        }
+        // step-3-2 true::>>  alert return
+
+    })
