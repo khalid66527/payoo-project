4ea2e79819d1c3acbfdbda8e217d5e227f4d5385
khalid hasan <khalidhasan678954321@gmail.com>
2026-03-02 02:07:38 +0600

    transsfer  success

---
+++ script/transfer.js
@@ -0,0 +1,25 @@
+document.getElementById("transfer-btn").addEventListener("click", function(){
+    const transferNumber = getValueFromInput("transfer-number");
+    if (transferNumber.length != 11){
+        alert("invalite Number")
+        return;
+    }
+    const transferAmount = getValueFromInput("transfer-amount")
+
+    const mainBalance = getBalance();
+
+    const newBalance = mainBalance - Number(transferAmount);
+     if(newBalance < 0){
+        alert("invalite Amount")
+        return;
+    }
+
+    const transferPin = getValueFromInput("transfer-pin")
+    if (transferPin === "1234"){
+        alert("Transfer Success")
+        setBalance(newBalance);
+    }
+    else{
+        alert("invalite pin")
+    }
+})
